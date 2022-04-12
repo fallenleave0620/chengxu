357c9311509955cd25abf501ea5b094b049f651c
fallenleave <hexiaoyu499@gmail.com>
2022-04-12 14:02:31 +0800

    Changed some files

--- 8.c
+++ 8.c
@@ -2,11 +2,8 @@
 #include"string.h"
 int main(){
     int *p;
-<<<<<<< HEAD
     int a=1;
     p=&a;
     printf("%x,%d",p,*p);
     return 0;
-=======
->>>>>>> 8708d9b62e5cfc0d7d3440ca9c40d687dea965d8
 }
