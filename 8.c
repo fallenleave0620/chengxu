4ef9c4078c9bd776309b676bc1ae10c35a3b90a9
fallenleave <hexiaoyu499@gmail.com>
2022-04-12 15:21:47 +0800

    Changed some files

--- 8.c
+++ 8.c
@@ -1,9 +1,19 @@
 #include"stdio.h"
 #include"string.h"
+#include"windows.h"
 int main(){
     int *p;
-    int a=1;
-    p=&a;
-    printf("%x,%d",p,*p);
+    int a[4],b[4];
+    // p=&a;
+    // printf("%x,%d",p,*p);
+    void test(int *a,int *b);
+    test(a,b);
+    system("pause");
     return 0;
+}
+void test(int *a,int *b){
+    for(int i=0;i<4;i++){
+        (a+i)=(b+i);
+    }
+    printf("%s\n%s",a,b);
 }
