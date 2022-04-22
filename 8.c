3b906b335c678c885220d268a3f82be821115a73
fallenleave <hexiaoyu499@gmail.com>
2022-04-22 10:14:46 +0800

    Changed some files

--- 8.c
+++ 8.c
@@ -1,6 +1,7 @@
 #include"stdio.h"
 #include"string.h"
 #include"windows.h"
+#include"bits/stdc++.h"
 int main(){
     int *p;
     int a[4]={0},b[4]={1,2,3,4};
@@ -8,12 +9,15 @@ int main(){
     // printf("%x,%d",p,*p);
     void test(int *a,int *b);
     test(a,b);
+    printf("\n");
     struct android{
-        char a[10]="version";
+        char a[10];
         int number;
         int year;
-    }android11;
-    android android10;
+    }android11={"android12",12,2020};
+    printf("android version=%s\nandroid num=%d\nandroid create=%d\n",
+    android11.a,android11.number,android11.year);
+    struct android android10;
     system("pause");
     return 0;
 }
